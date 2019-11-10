f18ed12779ae5cd0d9df44f6442f50c3fd47b973
sarah hildt <sarahhildt@gmail.com>
2019-11-09 22:17:00 -0700

    two player version

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,10 +1,13 @@
 #include <LedControl.h>
 // got library from arduino site
 
-const int SW_pin = 2; 
-const int X_pin = 0;
-const int Y_pin = 1;
-int ball_row = 7;
+const int SW_pin[2] = {2,3}; 
+const int X_pin[2] = {0,3};
+const int Y_pin[2] = {1,4};
+const int bar_row[2] = {0,7};
+
+
+int ball_row = 6;
 
 
 int row_off = B00000000;
@@ -15,70 +18,69 @@ bool going_left = true;
 bool at_row_end = false;
 bool at_row_top = false;
 
+bool p2wins = false;
 
 LedControl lc=LedControl(12,10,11,1);
-byte bar = B00011000;
+byte bar[2] = {B00011000,B00011000};
+
 byte ball = B00100000;
 byte prev_ball_val = ball;
+
+
 void setup() {
   init();
   Serial.begin(9600);
   lc.shutdown(0,false);
   lc.setIntensity(0,8);
   lc.clearDisplay(0);
-  pinMode(SW_pin, INPUT);
-  digitalWrite(SW_pin, HIGH);
-  lc.setRow(0,0,bar);
+  pinMode(SW_pin[0], INPUT);
+  digitalWrite(SW_pin[0], HIGH);
+  pinMode(SW_pin[1], INPUT);
+  digitalWrite(SW_pin[1], HIGH);
+  
+
+  lc.setRow(0,bar_row[0],bar[0]);
+  lc.setRow(0,bar_row[1],bar[1]);
   lc.setRow(0,ball_row,ball);
   
+  
 
   
   
 
 
-}
-int reverse_bar(int bar){
-  int reverse = 0;
-  int last_dig = B10000000;
-  for(int i=1;i<9;i++){
-    if((bar>>i&1)==1){
-      reverse = reverse | (last_dig>>i);
-      
-    }
-    
-  }
-  Serial.println(bar,BIN);
-  Serial.println(reverse,BIN);
-  return reverse;
 }
 
 
 
 void loop() {
-  
-
-  if(analogRead(X_pin)<=600 && analogRead(X_pin)>=500 ){
+  for(int i=0;i<2;i++){
+    if(analogRead(X_pin[i])<=600 && analogRead(X_pin[i])>=500 ){
     //Serial.println("not moving");
-    
-  }else if (analogRead(X_pin)<517){
-    //Serial.println("going Down");
-     if((bar & 1) !=1){
-      bar = bar>>1;
-      lc.setRow(0,0,bar);
       
-    }
-    
-  }else if (analogRead(X_pin)>517){
-    //Serial.println("going up");
-   
-     if((bar>>7 &1) !=1){
+    }else if (analogRead(X_pin[i])<517){
+      //Serial.println("going Down");
+       if((bar[i] & 1) !=1){
+        bar[i] = bar[i]>>1;
+        lc.setRow(0,bar_row[i],bar[i]);
         
-        bar = bar<<1;
+      }
       
-      lc.setRow(0,0,bar);
-      
+    }else if (analogRead(X_pin[i])>517){
+      //Serial.println("going up");
+     
+       if((bar[i]>>7 &1) !=1){
+          
+          bar[i] = bar[i]<<1;
+        
+        lc.setRow(0,bar_row[i],bar[i]);
+        
+      }
     }
+  
   }
+
+  
   
   if(!end_game){
     if((ball>>7 &1 )==1){
@@ -89,17 +91,37 @@ void loop() {
         going_left = true;
         
     }
-   if(ball_row==1 && ((ball<<1)&bar) ==0 && prev_ball_val>ball){
+   if(ball_row==1 && ((ball<<1)&bar[0]) ==0 && prev_ball_val>ball){
       Serial.println("ball right");
       Serial.println(ball,BIN);
       
       end_game = true; 
-   }else if(ball_row==1 && ((ball>>1)&bar) ==0 && prev_ball_val<ball){
+      p2wins = true;
+     
+   }else if(ball_row==1 && ((ball>>1)&bar[0]) ==0 && prev_ball_val<ball){
       Serial.println("ball left");
       Serial.println(ball,BIN);
       
       end_game = true; 
+      p2wins = true;
+      
+      
     
+   
+   }else if(ball_row==6 && ((ball<<1)&bar[1]) ==0 && prev_ball_val>ball){
+      Serial.println("ball right");
+      Serial.println(ball,BIN);
+      
+      end_game = true; 
+      p2wins = false;
+   }else if(ball_row==6 && ((ball>>1)&bar[1]) ==0 && prev_ball_val<ball){
+      Serial.println("ball left");
+      Serial.println(ball,BIN);
+      
+      end_game = true; 
+      p2wins = false;
+      
+      
    }else if(ball_row>1 && going_down){
      
       ball_row --; 
@@ -109,7 +131,7 @@ void loop() {
       lc.setRow(0,ball_row+1,row_off);
 
   
-    }else if(ball_row>=1 && ball_row<7){
+    }else if(ball_row>=1 && ball_row<6){
       if(ball_row==1){
         at_row_top = true;
       }else{
@@ -158,7 +180,8 @@ void loop() {
   
   
   
-  delay(200);
+  
+  delay(350);
   
 
 }
